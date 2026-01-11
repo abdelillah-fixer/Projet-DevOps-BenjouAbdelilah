e3cd4e6fb2e92c4979f1abc9eb1fc78b0d2ad943
abdelilah <benjouabdelilah@outlook.fr>
2026-01-11 18:17:18 +0100

    Ton message

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-        // Modification for dev branch
-        System.out.println("Bonjour et bon courage dans votre projet en DevOps");
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		// Modification for dev branch
+		System.out.println("Bonjour et bon courage dans votre projet en DevOps");
+	}
 }
