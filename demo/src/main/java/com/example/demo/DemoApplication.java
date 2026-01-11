d1efe7b4feeecfb0cbb256b355f8c806c7b969b5
abdelilah <benjouabdelilah@outlook.fr>
2026-01-11 17:25:34 +0100

    Add comment in dev branch

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        // Modification for dev branch
         System.out.println("Bonjour et bon courage dans votre projet en DevOps");
     }
 }
